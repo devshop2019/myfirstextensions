2161f381b8a3f64f90e25e38f079b02989b7896a
devshop2019 <50487904+devshop2019@users.noreply.github.com>
2022-03-04 17:58:32 +0700

    test MA run OK

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
 
 enum MKL_DRIVER_I2C_MOTOR_INDEX{
     //% block="Driver 1"
-    MKL_DRIVER_I2C_MOTOR_INDEX_1,
+    MKL_DRIVER_I2C_MOTOR_INDEX_1 = 64,
     //% block="Driver 2"
     MKL_DRIVER_I2C_MOTOR_INDEX_2,
     //% block="Driver 3"
@@ -32,6 +32,13 @@ enum MKL_DRIVER_I2C_MOTOR_MODE_ID{
     DC_ID,
     SADDR_ID,
     END_MODE_ID
+}
+
+enum MKL_MOTOR_STOP_MODE{
+    //% block="no brake"
+    noBrake,
+    //% block="brake"
+    brake
 }
 
 //% color="#ff0000" icon="\uf085"
@@ -89,21 +96,82 @@ namespace MKLModules {
     //% block="Set $motorI2C_Address_index|motor $Motor_I2C_DC_index|go %Motor_I2C_DC_dir_index|with speed $speed"
     //% speed.min=0 speed.max=100
     export function Set_MKL_I2C_MotorDriver(motorI2C_Address_index: MKL_DRIVER_I2C_MOTOR_INDEX, Motor_I2C_DC_index: MKL_DRIVER_I2C_MOTOR_DC_INDEX, Motor_I2C_DC_dir_index:MKL_DRIVER_I2C_MOTOR_DC_DIR_INDEX, speed: number): number{
-        let tempAddr = motorI2C_Address_index - MKL_DRIVER_I2C_MOTOR_INDEX.MKL_DRIVER_I2C_MOTOR_INDEX_1;
+        let tempAddr = motorI2C_Address_index;
         let tempModeID = MKL_DRIVER_I2C_MOTOR_MODE_ID.DC_ID - MKL_DRIVER_I2C_MOTOR_MODE_ID.RC_ID;
-        let tempIndex = 0;
-        let tempPwm = 0;
-        let tempDir = 0;
+        let tempIndex = Motor_I2C_DC_index - MKL_DRIVER_I2C_MOTOR_DC_INDEX.MA;
+        let tempPwm = (speed&0xff)*255/100;
+        let tempDir = Motor_I2C_DC_dir_index - MKL_DRIVER_I2C_MOTOR_DC_DIR_INDEX.forward;
 
         
-        let i2cData = pins.createBuffer(MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE)
+        SendDataTo_MKL_I2C_Driver(tempAddr, tempModeID,tempIndex,tempPwm,tempDir);
 
-        i2cData[0] = 0;
+        // let i2cData = pins.createBuffer(MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE);
+
+        // i2cData[0] = tempAddr;
+        // i2cData[1] = tempModeID;
+        // i2cData[2] = tempIndex;
+        // i2cData[3] = tempPwm;
+        // i2cData[4] = tempDir;
         
-        i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] = 0;
-        for (let cf = 0; cf < MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1; cf++){
-            i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] += i2cData[cf]
-        }
+        // i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] = 0;
+        // for (let cf = 0; cf < MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1; cf++){
+        //     i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] += i2cData[cf];
+        // }
+        // i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] = i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] & 0xff;
+
         return tempAddr;
+    }
+
+    //% block="Set $motorI2C_Address_index|motor $Motor_I2C_DC_index|stop with %"
+    export function Set_MKL_I2C_MotorDriver_Stop(motorI2C_Address_index: MKL_DRIVER_I2C_MOTOR_INDEX, Motor_I2C_DC_index: MKL_DRIVER_I2C_MOTOR_DC_INDEX, stopMode_:MKL_MOTOR_STOP_MODE): number {
+        let tempAddr = motorI2C_Address_index;
+        let tempModeID = MKL_DRIVER_I2C_MOTOR_MODE_ID.DC_ID - MKL_DRIVER_I2C_MOTOR_MODE_ID.RC_ID;
+        let tempIndex = Motor_I2C_DC_index - MKL_DRIVER_I2C_MOTOR_DC_INDEX.MA;
+
+        // mode brake
+        let tempPwm = 0;
+        let tempDir = MKL_DRIVER_I2C_MOTOR_DC_DIR_INDEX.backward;
+
+        if(stopMode_ == MKL_MOTOR_STOP_MODE.noBrake){
+            tempPwm = 0;
+            tempDir = MKL_DRIVER_I2C_MOTOR_DC_DIR_INDEX.forward;
+        }
+
+        SendDataTo_MKL_I2C_Driver(tempAddr, tempModeID, tempIndex, tempPwm, tempDir);
+
+        return tempAddr;
+    }
+
+    export function SendDataTo_MKL_I2C_Driver(_address_:number, _modeID_:number, _index_:number, _pwm_:number, _dir_:number):void{
+        let i2cData = pins.createBuffer(MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE);
+
+        i2cData[0] = _address_;
+        i2cData[1] = _modeID_;
+        i2cData[2] = _index_;
+        i2cData[3] = _pwm_;
+        i2cData[4] = _dir_;
+
+        // serial.writeNumber(0)
+        // serial.writeLine(" Start send i2cData")
+
+        i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] = 0;
+        for (let cf = 0; cf < MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1; cf++) {
+            i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] += i2cData[cf];
+            // serial.writeNumber(i2cData[cf])
+            // serial.writeLine("")
+        }
+        i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] = i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1] & 0xff;
+        // serial.writeNumber(i2cData[MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE - 1])
+        // serial.writeLine(" END")
+        pins.i2cWriteBuffer(_address_, i2cData, false);
+    }
+
+    //% block="Get address Driver I2C"
+    export function scan_I2C_Driver_Address():number{
+        for (let cf = MKL_DRIVER_I2C_MOTOR_INDEX.MKL_DRIVER_I2C_MOTOR_INDEX_1; cf < (MKL_DRIVER_I2C_MOTOR_INDEX.MKL_DRIVER_I2C_MOTOR_INDEX_1 + MKL_DRIVER_I2C_MOTOR_MAX_BUFFER_SIZE -1); cf++){
+            let tempValue = pins.i2cReadNumber(cf, NumberFormat.Int8LE, false);
+            if(tempValue != 0) return cf;
+        }
+        return 0;
     }
 }
